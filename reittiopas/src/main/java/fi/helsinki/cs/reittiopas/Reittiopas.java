66c11f315d476b48f7ea1b5a0c9eb31121efa9e3
Johannes Elmnäinen <sanhologin@gmail.com>
2017-01-19 16:11:55 +0200

    init Reittiopas

---
+++ reittiopas/src/main/java/fi/helsinki/cs/reittiopas/Reittiopas.java
@@ -0,0 +1,23 @@
+package fi.helsinki.cs.reittiopas;
+
+public class Reittiopas {
+
+    /**
+     * Toteuta leveyssuuntainen haku. Palauta reitti taaksepäin linkitettynä
+     * listana Tila-olioita, joista ensimmäinen osoittaa maalipysäkkiin ja
+     * jokainen tuntee pysäkin ja tilan, josta kyseiseen tilaan päästiin
+     * (viimeisen solmun Pysäkki on lähtöpysäkki ja edellinen tila on null).
+     *
+     * Voit selvittää pysäkin naapuripysäkit, eli pysäkit joihin pysäkiltä on
+     * suora yhteys, kutsumalla pysäkin getNaapurit() -metodia.
+     *
+     * @param lahto Lahtopysakin koodi
+     * @param maali Maalipysakin koodi
+     * @return Tila-olioista koostuva linkitetty lista maalista lähtötilaan
+     */
+    public Tila haku(Pysakki lahto, Pysakki maali) {
+        //toteuta leveyssuuntainen haku
+        //Tila tila = new Tila(lahto, null);
+        return null;
+    }
+}
