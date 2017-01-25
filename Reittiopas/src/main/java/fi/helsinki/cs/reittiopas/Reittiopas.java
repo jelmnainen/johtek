175028c3c7325c84ece5ca5ec3d03b5a3f332507
Johannes Elmnäinen <jelmnain@cs.helsinki.fi>
2017-01-25 16:11:49 +0200

    implement Reittiopas

--- Reittiopas/src/main/java/fi/helsinki/cs/reittiopas/Reittiopas.java
+++ Reittiopas/src/main/java/fi/helsinki/cs/reittiopas/Reittiopas.java
@@ -1,5 +1,10 @@
 package fi.helsinki.cs.reittiopas;
 
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Reittiopas {
 
     /**
@@ -16,8 +21,30 @@ public class Reittiopas {
      * @return Tila-olioista koostuva linkitetty lista maalista lähtötilaan
      */
     public Tila haku(Pysakki lahto, Pysakki maali) {
-        //toteuta leveyssuuntainen haku
-        //Tila tila = new Tila(lahto, null);
+        ArrayDeque<Tila> solmulista = new ArrayDeque();
+        HashSet<Pysakki> kasitellyt = new HashSet();
+        
+        solmulista.add(new Tila(lahto, null));
+        while(!solmulista.isEmpty()) {
+            Tila solmu = solmulista.poll();
+            Pysakki solmunPysakki = solmu.getPysakki();
+            kasitellyt.add(solmunPysakki);
+            if (solmunPysakki.equals(maali)) {
+                return solmu;
+            }
+            solmulista = lisaa(solmunPysakki.getNaapurit(), solmulista, solmu, kasitellyt);    
+        }
+        
         return null;
     }
+
+    private ArrayDeque<Tila> lisaa(Collection<Pysakki> naapurit, ArrayDeque<Tila> solmulista, Tila edellinen, HashSet<Pysakki> kasitellyt) {
+        for (Pysakki pysakki : naapurit) {
+            if (!kasitellyt.contains(pysakki)) {
+                solmulista.add(new Tila(pysakki, edellinen));
+            }
+        }
+        return solmulista;
+    }
+
 }
